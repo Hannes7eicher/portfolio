86501f26185a4db82a7a256f54e126504f783ea7
Hannes Siebeneicher <hannes7eicher@gmail.com>
2023-03-20 22:58:56 +0100

    add new background animation

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,11 @@ export default function Home() {
     return(
       <main>
         <div id="body" className='bg-white'>
-            <svg className="absolute animate-spin h-screen w-full blur-3xl z-0">
-              <circle className='animate-pulse' cx="40%" cy="30%" r="25%" fill="#9C95DC" opacity="0.08" />
-              <circle className='animate-pulse' cx="20%" cy="50%" r="15%" fill="#C19AB7" opacity="0.1" />
-              <circle className='animate-pulse' cx="70%" cy="70%" r="20%" fill="#730071" opacity="0.1" />
-              <circle className='animate-pulse' cx="10%" cy="70%" r="20%" fill="#730071" opacity="0.04" />
+            <svg className="absolute h-full animate-spin w-full blur-3xl z-0">
+              <circle className='animate-pulse' cx="40%" cy="30%" r="25%" fill="hsla(268,70%,71%,1)" opacity="0.08" />
+              <circle className='animate-pulse' cx="20%" cy="50%" r="15%" fill="hsla(39,93%,68%,1)" opacity="0.1" />
+              <circle className='animate-pulse' cx="70%" cy="70%" r="20%" fill="hsla(239,68%,64%,1)" opacity="0.1" />
+              <circle className='animate-pulse' cx="10%" cy="70%" r="20%" fill="hsla(226,83%,78%,1)" opacity="0.04" />
             </svg>
             <div className="">
               <div className="mx-auto max-w-2xl py-32 sm:py-64">
@@ -20,8 +20,8 @@ export default function Home() {
               </p>
                 </div>
               </div>
+              <Section/>
         </div>
-        <Section/>
     </main>
   );
 }
--- src/Section.js
+++ src/Section.js
@@ -2,9 +2,9 @@ import Card from './Card'
 
 export default function Section() {
     return (
-      <div className="grid-container grid grid-cols-1 justify-items-center gap-y-32 sm:gap-y-64 p-8 bg-white">
-          <Card title={"Arduino Plant Watering Kit"} subtitle={"Check on your plants from anywhere even when you're not around."} image={"plantKit.png"} />
-          <Card title={"Arduino IoT Energy Meter"} subtitle={"Become more aware of consumption by monitoring your energy meter with an Arduino."} image={"energyMeter.png"} />
+      <div className="relative grid-container grid grid-cols-1 justify-items-center gap-y-32 sm:gap-y-64 p-8 bg-transparent">
+          <Card title="Arduino Plant Watering Kit" subtitle="Check on your plants from anywhere even when you're not around." image="plantKit.png" />
+          <Card title="Arduino IoT Energy Meter" subtitle="Become more aware of consumption by monitoring your energy meter with an Arduino." image="energyMeter.png" />
 
       </div>
     );
