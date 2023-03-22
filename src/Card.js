1fb08e3ff0b78c1ad79c3e2fc36fa70a55f63924
Hannes Siebeneicher <hannes7eicher@gmail.com>
2023-03-22 23:54:28 +0100

    return to old animation

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,13 @@ export default function Card({image, title, subtitle}) {
 
   console.log(image);
     const src = require("./assets/" + image);
+
+    const handleClick = () => {
+      
+    }
     
     return (
-<div>
+<div id="card" className="cursor-pointer" onClick={handleClick}>
   <div>
     <h1 class="text-xl lg:text-4xl font-bold leading-none tracking-tight text-gray-700"> {title} </h1>
     <p class="my-4 text-lg text-gray-500"> {subtitle} </p>
--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,12 @@ export default function Home() {
     return(
       <main>
         <div id="body" className='bg-white'>
-            <svg className="absolute animate-spin h-screen w-full blur-3xl z-0">
-              <circle className='animate-pulse' cx="40%" cy="30%" r="25%" fill="#9C95DC" opacity="0.08" />
-              <circle className='animate-pulse' cx="20%" cy="50%" r="15%" fill="#C19AB7" opacity="0.1" />
-              <circle className='animate-pulse' cx="70%" cy="70%" r="20%" fill="#730071" opacity="0.1" />
-              <circle className='animate-pulse' cx="10%" cy="70%" r="20%" fill="#730071" opacity="0.04" />
+          <div className='absolute h-screen w-full blur-3xl'>
+            <svg className="absolute sm:animate-spin h-full w-full">
+              <circle className='animate-pulse' cx="45%" cy="20%" r="10%" fill="#9C95DC" opacity="0.08" />
+              <circle className='animate-pulse' cx="55%" cy="35%" r="10%" fill="#C19AB7" opacity="0.1" />
             </svg>
+            </div>
             <div className="">
               <div className="mx-auto max-w-2xl py-32 sm:py-64">
                   <h1 className="relative text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center sm:text-center">Hannes Siebeneicher</h1>
@@ -20,8 +20,8 @@ export default function Home() {
               </p>
                 </div>
               </div>
-        </div>
         <Section/>
+        </div>
     </main>
   );
 }
