e9f97ed59b5d24b365f7b6e3b36e913e14f0a3fa
hannes7eicher <hannes7eicher@gmail.com>
2023-04-16 15:35:38 +0200

    changing position

--- src/components/Home/HomeHero/HomeHero.js
+++ src/components/Home/HomeHero/HomeHero.js
@@ -5,17 +5,23 @@ export default function HomeHero() {
   return (
     <div id="body">
       <div className='absolute h-screen w-full blur-3xl'>
-        <svg className="absolute sm:animate-spin h-full w-full">
-          <circle className='animate-pulse' cx="45%" cy="20%" r="10%" fill="#9C95DC" opacity="0.8" />
-          <circle className='animate-pulse' cx="55%" cy="35%" r="10%" fill="#C19AB7" opacity="0.1" />
+        <svg className="absolute animate-spin h-full w-full">
+          <circle className='animate-pulse' cx="25%" cy="20%" r="10%" fill="#9C95DC" opacity="1" />
+          <circle className='animate-pulse' cx="25%" cy="35%" r="10%" fill="#C19AB7" opacity="1" />
         </svg>
         </div>
         <div>
-          <div className="mx-auto max-w-2xl py-32 sm:py-64">
-              <h1 className="relative text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center font-PlayfairDisplay sm:text-center">Hannes Siebeneicher</h1>
-              <p className="relative py-8 text-lg leading-8 text-gray-600 text-center font-RobotoCondensed sm:text-center">
-            crafting interactions
+          <div className="mx-auto max-w-2xl pt-96 pb-32 ml-0">
+              <h1 className="relative text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-left font-PlayfairDisplay pl-8 pb-0">Hannes Siebeneicher</h1>
+              <p className="relative text-lg leading-8 text-gray-600 text-left font-RobotoCondensed pl-8 pb-0">
+                
+            Creative technologiest crafting interactions
           </p>
+
+          <p className="relative text-lg leading-8 text-gray-400 text-left  font-RobotoCondensed pl-8 pb-0">
+                
+                Currently working at Arduino
+              </p>
             </div>
           </div>
     </div>
--- src/components/Home/HomeSection/HomeSection.js
+++ src/components/Home/HomeSection/HomeSection.js
@@ -2,7 +2,7 @@ import HomeCard from "./HomeCard/HomeCard";
 
 export default function HomeSection() {
     return (
-      <div className="relative grid-container grid grid-cols-1 justify-items-center gap-y-32 sm:gap-y-64 p-8 bg-transparent">
+      <div className="relative grid-container grid grid-cols-1 justify-items-left sm:justify-items-center px-8 gap-y-32 sm:gap-y-64 pb-32 bg-transparent">
           <HomeCard 
           title="Arduino Plant Watering Kit" 
           subtitle="Check on your plants from anywhere even when you're not around." 
@@ -10,7 +10,7 @@ export default function HomeSection() {
           route="/Plantkit"
           />
           <HomeCard 
-          title="Arduino IoT Energy Meter" 
+          title="Arduino IoT Energy Meter"
           subtitle="Become more aware of consumption by monitoring your energy." 
           image="energyMeter.png" 
           route="/EnergyMeter"
