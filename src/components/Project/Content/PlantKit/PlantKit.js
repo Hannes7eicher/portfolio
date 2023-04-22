cfa06ec5077ffb9482e1043499fcfc36fde5da98
Hannes7eicher <hannes7eicher@gmail.com>
2023-04-22 11:49:28 +0200

    fix image size

--- src/components/Project/Content/PlantKit/PlantKit.js
+++ src/components/Project/Content/PlantKit/PlantKit.js
@@ -6,7 +6,12 @@ export default function PlantKit() {
 
   return (
     <motion.div>
-        <ProjectMain title="Plant Watering Kit" subtitle="Water your plants from anywhere and anytime!" image="plantKit.png" />
+        <ProjectMain 
+        title="Plant Watering Kit" 
+        subtitle="Water your plants from anywhere and anytime!" 
+        image="plantKit.png" 
+        content="During my time at Arduino I have had the chance to work on conceptualizing, prototyping and launching a cloud enabled and remote controllable plant watering system." 
+        />
     </motion.div>
   );
 }
--- src/components/Project/ProjectMain/ProjectMain.js
+++ src/components/Project/ProjectMain/ProjectMain.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 
-export default function ProjectMain({image, title, subtitle}) {
+export default function ProjectMain({image, title, subtitle, content}) {
 
     const navigate = useNavigate();
 
@@ -12,15 +12,12 @@ export default function ProjectMain({image, title, subtitle}) {
           <div className="hero-content text-center text-neutral-content">
             <div className="max-w-sm sm:max-w-xl">
               <h1 className="mt-10 mb-5 text-4xl sm:text-6xl text-left font-bold tracking-tight text-gray-700">{title}</h1>
-              <p className="mb-5 text-xl sm:text-2xl text-left tracking-tight text-gray-700">{subtitle}</p>
+              <p className="mb-5 text-xl sm:text-2xl text-left tracking-tight text-gray-700"> { subtitle } </p>
               <button onClick = {() => navigate('/')}   className="absolute sm:fixed text-gray-500 font-bold top-5 left-5 ">X</button>
 
               <img className='mb-10' src={src} alt="" />
 
-              <p className="my-10 text-left text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et lacinia dui. Nam mollis pretium ligula sed auctor. Donec suscipit commodo eleifend. Maecenas nec mi tempor, posuere lacus lacinia, rutrum tortor. Sed ultrices, orci eu porttitor tincidunt, ligula leo fermentum risus, vel eleifend eros elit non ligula. Vestibulum tempus ut odio eget suscipit. Vivamus elementum augue eu orci varius, quis finibus mauris maximus. Aenean eros libero, laoreet id sagittis vel, semper sed leo.
-                Curabitur rutrum lacus et dignissim viverra. Proin lectus ligula, laoreet at ante ac, scelerisque rhoncus justo. Quisque nibh nunc, tristique et dapibus egestas, posuere vitae tellus. Morbi. 
-              </p>
+              <p className="my-10 text-left text-gray-700"> { content } </p>
 
             </div>
           </div>
